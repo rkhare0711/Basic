8a3970f4dd9f2e97157cdd533ccad88af81fef31
rkhare0711 <rkhare0711@gmail.com>
2022-02-05 11:25:58 +0530

    Basic Practise

---
+++ src/linkedList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class linkedList {
+    public static void main(String[] args) {
+        List<Integer> l1=new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        System.out.println(l1);
+    }
+}
